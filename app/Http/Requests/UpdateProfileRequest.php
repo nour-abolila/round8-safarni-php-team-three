23c3e18e10648d6636ca14e707b04868f961cf86
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-01-07 16:22:57 +0200

    upate_otp_code_and_add_show_profile_and_update_profile

---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'full_name' => ['sometimes', 'string', 'max:255'],
+            'email' => [
+                'sometimes',
+                'email',
+                'max:255',
+                'unique:users,email,' .  Auth::id(),
+            ],
+            'mobile' => ['sometimes', 'string', 'max:20'],
+        ];
+    }
+}
